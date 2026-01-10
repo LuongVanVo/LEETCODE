928d5ef1d419512f0d53661ae2729824c2ffe9ae
Luong Van Vo <142654582+LuongVanVo@users.noreply.github.com>
2026-01-11 00:22:19 +0700

    Time: 14 ms (91.92%), Space: 46.3 MB (35.1%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int CoinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Array.Fill(dp, amount + 1);
+
+        dp[0] = 0; 
+
+        foreach (int coin in coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = Math.Min(dp[j], dp[j - coin] + 1);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
